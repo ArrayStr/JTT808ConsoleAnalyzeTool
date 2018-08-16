9ef206453f9fd0a66b227ddf04d15e9f8ba77845
cc <ccpublic@foxmail.com>
2018-08-16 10:33:18 +0800

    test

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ConsoleApp2;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
